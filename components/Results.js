678a020d56a28183439cb68bc2e09ee1bec81a32
gyaneshrn <gyaneshrn94@gmail.com>
2021-09-10 14:48:28 +0530

    minor changes for Thumbnail

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,14 @@
 import Thumbnail from "./Thumbnail";
+import FlipMove from 'react-flip-move';
 
 function Results({results}) {
-    console.log('>>> result', results);
     return (
-        <div className="px-5 my-10 sm:grid md:grid-col-2 xl:grid-cols-3
+        <FlipMove className="px-5 my-10 sm:grid md:grid-col-2 xl:grid-cols-3
         3xl:flex flex-wrap justify-center ">
             {results.map((result)=>(
                   <Thumbnail key={result.id} result={result}/>
             ))}
-        </div>
+        </FlipMove>
     );
 }
 
